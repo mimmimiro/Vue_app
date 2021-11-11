ca34b3020da35525f0ba74dd894eb0beefdd477c
mimmimiro <amini.maryam@gmail.com>
2021-11-11 14:39:44 +0100

    first

---
+++ src/app/App.vue
@@ -0,0 +1,17 @@
+<template>
+	<counter />
+</template>
+
+<script>
+import Counter from'../components/Counter.vue'
+export default {
+	components: {
+		Counter,
+		},
+		};
+</script>
+
+<style>
+@import '../style/variables.css';
+@import '../style/style.css'; 
+</style> 
