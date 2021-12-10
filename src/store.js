d8d15284d933d44912d7991ecd152afd7e96707e
mimmimiro <amini.maryam@gmail.com>
2021-12-10 12:51:07 +0100

    f

--- src/store.js
+++ src/store.js
@@ -1,28 +1,5 @@
 export default {
-	State() {
-		return {
-			pages: [
-			{ title: 'kontakt oss', id: '' }, // fyll inn resterende
-			{ title: 'quiz', id: '' },
-			{ title: 'slideshow', id: '' },
-			{ title: 'table', id: '' },
-			{ title: 'to do list', id: '' },
-			]
-		}
-	}, 
-
-
-/*
-getters: {
-	getPages(state) {
-		return state.pages;
-	}
+	state() {
+		return {}
 	},
-	mutations: {
-		addPage(state, page) {
-			state.navigation.pages.push(page)
-		}
-	}
-
-*/
-}
+};
