689c253720a8ad4d1731b405de4d5e3f5aca3f0d
mimmimiro <amini.maryam@gmail.com>
2022-01-31 10:22:15 +0100

    Added formatting

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,59 +1,56 @@
 <template class="media">
 	
 		<nav class="header">
-		 <RouterLink class="header__navigation" :to="{ name: 'home', Path: '/', component: Home }">Home</RouterLink>
-		
-		 <RouterLink class="header__navigation" :to="{ name: 'contact', path: '/Contact', component: Contact }">Contact</RouterLink>
-		 <RouterLink class="header__navigation" :to="{ name: 'table', path: '/Table', component: Table }">Table</RouterLink>
-		 <RouterLink class="header__navigation" :to="{ name: 'todolist', path: '/ToDoList', component: ToDoList }">ToDoList</RouterLink>
-		 <RouterLink class="header__navigation" :to="{ name: 'quiz', path: '/Quiz', component: Quiz }">Quiz</RouterLink>
+		  <RouterLink class="header__navigation" :to="{ name: 'home', Path: '/', component: Home }">Home</RouterLink>
+		  <RouterLink class="header__navigation" :to="{ name: 'contact', path: '/Contact', component: Contact }">Contact</RouterLink>
+		  <RouterLink class="header__navigation" :to="{ name: 'table', path: '/Table', component: Table }">Table</RouterLink>
+		  <RouterLink class="header__navigation" :to="{ name: 'todolist', path: '/ToDoList', component: ToDoList }">ToDoList</RouterLink>
+		  <RouterLink class="header__navigation" :to="{ name: 'quiz', path: '/Quiz', component: Quiz }">Quiz</RouterLink>
 		  <RouterLink class="header__navigation" :to="{ name: 'slideshow', path: '/Slideshow', component: Slideshow }">Slideshow</RouterLink>
 		</nav>
 	
-	<Routerview />
+	 <Routerview />
 </template>
+
 <script>
-export default {
-	data () {
-	 return {
-		
-	}
-},
-
-
+  export default {
+    data () {
+	   return {
+	 }
+  },
 };
 </script>
 
 <style>
     
-.header {
-	padding: 30px;
-	display: flex;
-	justify-content: space-between;
-	text-decoration: none;
-	background: linear-gradient(to left, rgb(228, 227, 227), rgb(216, 173, 146) ) ;
+  .header {
+    padding: 30px;
+	 display: flex;
+	 justify-content: space-between;
+	 text-decoration: none;
+	 background: linear-gradient(to left, rgb(228, 227, 227), rgb(216, 173, 146) ) ;
 	
-}
+  }
 
-.header__navigation {
-	color: darkcyan;
-	font-family: Verdana, Geneva, Tahoma, sans-serif;
-	font-size: 25px;
-	text-decoration: none;
-}
+  .header__navigation {
+	  color: darkcyan;
+	  font-family: Verdana, Geneva, Tahoma, sans-serif;
+	  font-size: 25px;
+	  text-decoration: none;
+  }
 
-.header__navigation:hover {
-	color: #ea532a;;
-}
+  .header__navigation:hover {
+	  color: #ea532a;;
+  }
 
-@media (max-width: 675px) {
-	nav  {
-		flex-direction: column;
-	}
+    @media (max-width: 675px) {
+	   nav  {
+		  flex-direction: column;
+	   }
 
-	.header__navigation {
-		margin: 0.5em 0.5em;
-	}
-}
+	   .header__navigation {
+		  margin: 0.5em 0.5em;
+	   }
+  }
 
 </style>
