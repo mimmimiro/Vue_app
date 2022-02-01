42944704a4fe2725dc04661406fcfb70ed73893c
mimmimiro <amini.maryam@gmail.com>
2022-02-01 13:49:01 +0100

    Added formatting

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,27 +2,25 @@
 legge dem på forsiden, men jeg synes dette ble mye kulere.-->
 <template>
 	<section class="home">
- <RouterLink class="home__contact" :to="{ name: 'contact', path: '/Contact', component: Contact }">Contact</RouterLink>
- <RouterLink class="home__todolist" :to="{ name: 'todolist', path: '/ToDoList', component: ToDoList }">ToDoList</RouterLink>
-<RouterLink class="home__quiz" :to="{ name: 'quiz', path: '/Quiz', component: Quiz }">Quiz</RouterLink>
- <RouterLink class="home__slideshow" :to="{ name: 'slideshow', path: '/Slideshow', component: Slideshow }">Slideshow</RouterLink>
- <RouterLink class="home__table" :to="{ name: 'table', path: '/Table', component: Table }">Table</RouterLink>
-</section>
-<footer class="article">
-	<h1 class="article__title"> The invention of the app </h1>
-	<h5 class="article__brand">@CartaDesign</h5>
-</footer>
+     <RouterLink class="home__contact" :to="{ name: 'contact' }">Contact</RouterLink>
+     <RouterLink class="home__todolist" :to="{ name: 'todolist' }">ToDoList</RouterLink>
+     <RouterLink class="home__quiz" :to="{ name: 'quiz' }">Quiz</RouterLink>
+     <RouterLink class="home__slideshow" :to="{ name: 'slideshow' }">Slideshow</RouterLink>
+    <RouterLink class="home__table" :to="{ name: 'table' }">Table</RouterLink>
+   </section>
+
+  <footer class="article">
+    <h1 class="article__title"> The invention of the app </h1>
+	 <h5 class="article__brand">@CartaDesign</h5>
+  </footer>
 </template>
 
 <script>
 
 	export default {
-		components: {
-			   
-				
-    
+		components: {	
+
 		},
-	
 	};
 
 </script>
@@ -30,115 +28,115 @@ legge dem på forsiden, men jeg synes dette ble mye kulere.-->
 <style>
 
 
-.home__contact { 
-  display: inline-block;
-	background: #da552f;
-	width: 230px;
-	height: 230px;
-	margin: 60px;
-	border-radius: 130px;
-	text-align: center;
-	font-size: 22px;
-	font-weight: bold;
-	padding-top: 130px;
-	text-decoration: none;
-	color: rgb(3, 82, 82);
-	font-family: var(--font-family);
+  .home__contact { 
+    display: inline-block;
+	 background: #da552f;
+	 width: 230px;
+	 height: 230px;
+	 margin: 60px;
+	 border-radius: 130px;
+	 text-align: center;
+	 font-size: 22px;
+	 font-weight: bold;
+	 padding-top: 130px;
+	 text-decoration: none;
+	 color: rgb(3, 82, 82);
+	 font-family: var(--font-family);
 	}
 	
 	.home__todolist {
-	display: inline-block;
-	background: #da552f;
-	width: 280px;
-	height: 280px;
-	margin: 60px;
-	border-radius: 180px;
-	text-align: center;
-	line-height: 190px;
-	font-size: 20px;
-	font-weight: bold;
-	text-decoration: none;
-	color: rgb(3, 82, 82);
-	font-family: var(--font-family);
+	  display: inline-block;
+	  background: #da552f;
+	  width: 280px;
+	  height: 280px;
+	  margin: 60px;
+	  border-radius: 180px;
+	  text-align: center;
+	  line-height: 190px;
+	  font-size: 20px;
+	  font-weight: bold;
+	  text-decoration: none;
+	  color: rgb(3, 82, 82);
+	  font-family: var(--font-family);
 	}
 
-.home__todolist:hover {
-	color: blanchedalmond;
-}
+   .home__todolist:hover {
+	  color: blanchedalmond;
+   }
+
 	.home__quiz {
-	display: inline-block;
-	background: #da552f;
-	width: 250px;
-	height: 250px;
-	margin: 60px;
-	border-radius: 150px;
-	text-align: center;
-	line-height: 190px;
-	font-size: 20px;
-	font-weight: bold;
-	text-decoration: none;
-	color: rgb(3, 82, 82);
-	font-family: var(--font-family);
+	  display: inline-block;
+	  background: #da552f;
+	  width: 250px;
+	  height: 250px;
+	  margin: 60px;
+	  border-radius: 150px;
+	  text-align: center;
+	  line-height: 190px;
+	  font-size: 20px;
+	  font-weight: bold;
+	  text-decoration: none;
+	  color: rgb(3, 82, 82);
+	  font-family: var(--font-family);
 	}
 
 	.home__quiz:hover {
-	color: blanchedalmond;
-}
+	  color: blanchedalmond;
+   }
 	
 
 	.home__slideshow {
-	display: inline-block;
-	background: #da552f;
-	width: 250px;
-	height: 250px;
-	margin: 60px;
-	border-radius: 150px;
-	text-align: center;
-	line-height: 20px;
-	font-size: 25px;
-	font-weight: bold;
-	padding-top: 130px;
-	text-decoration: none;
-	color: rgb(3, 82, 82);
-	font-family: var(--font-family);
+	  display: inline-block;
+	  background: #da552f;
+	  width: 250px;
+	  height: 250px;
+	  margin: 60px;
+	  border-radius: 150px;
+	  text-align: center;
+	  line-height: 20px;
+	  font-size: 25px;
+	  font-weight: bold;
+	  padding-top: 130px;
+	  text-decoration: none;
+	  color: rgb(3, 82, 82);
+	  font-family: var(--font-family);
 	}
+
    .home__slideshow:hover {
-	
-	color: blanchedalmond;
+	  color: blanchedalmond;
+   }
 
- }
 	.home__table {
-	display: inline-block;
-	background: #da552f;
-	width: 230px;
-	height: 230px;
-	margin: 60px;
-	border-radius: 130px;
-	text-align: center;
-	line-height: 30px;
-	font-size: 20px;
-	font-weight: bold;
-	padding-top: 150px;
-	text-decoration: none;
-	color: rgb(3, 82, 82);
-	font-family: var(--font-family);
+	  display: inline-block;
+	  background: #da552f;
+	  width: 230px;
+	  height: 230px;
+	  margin: 60px;
+	  border-radius: 130px;
+	  text-align: center;
+	  line-height: 30px;
+	  font-size: 20px;
+	  font-weight: bold;
+	  padding-top: 150px;
+	  text-decoration: none;
+	  color: rgb(3, 82, 82);
+	  font-family: var(--font-family);
 	}
-	.home__table:hover {
-		
-	color: blanchedalmond;
-}
+
+	.home__table:hover {	
+	  color: blanchedalmond;
+  }
 	
-
-.home__contact:hover
- {
-		color: blanchedalmond;
+   .home__contact:hover {
+	  color: blanchedalmond;
 	}
 
-.article__title {
-	border-top: solid 4px darkcyan;
-}
-.article__brand {
-	color: darkcyan;
-}
+  .article__title {
+	  border-top: solid 4px darkcyan;
+  
+  }
+  .article__brand {
+	  color: darkcyan;
+  }
 	
 </style>
