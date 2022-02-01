b0b62b0540707f5126745e3fef656e826eb26017
mimmimiro <amini.maryam@gmail.com>
2022-02-01 13:41:20 +0100

    Added formatting

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,12 @@
 <template class="media">
 	
 		<nav class="header">
-		  <RouterLink class="header__navigation" :to="{ name: 'home', Path: '/', component: Home }">Home</RouterLink>
-		  <RouterLink class="header__navigation" :to="{ name: 'contact', path: '/Contact', component: Contact }">Contact</RouterLink>
-		  <RouterLink class="header__navigation" :to="{ name: 'table', path: '/Table', component: Table }">Table</RouterLink>
-		  <RouterLink class="header__navigation" :to="{ name: 'todolist', path: '/ToDoList', component: ToDoList }">ToDoList</RouterLink>
-		  <RouterLink class="header__navigation" :to="{ name: 'quiz', path: '/Quiz', component: Quiz }">Quiz</RouterLink>
-		  <RouterLink class="header__navigation" :to="{ name: 'slideshow', path: '/Slideshow', component: Slideshow }">Slideshow</RouterLink>
+		  <RouterLink class="header__navigation" :to="{ name: 'home' }">Home</RouterLink>
+		  <RouterLink class="header__navigation" :to="{ name: 'contact' }">Contact</RouterLink>
+		  <RouterLink class="header__navigation" :to="{ name: 'table' }">Table</RouterLink>
+		  <RouterLink class="header__navigation" :to="{ name: 'todolist' }">ToDoList</RouterLink>
+		  <RouterLink class="header__navigation" :to="{ name: 'quiz' }">Quiz</RouterLink>
+		  <RouterLink class="header__navigation" :to="{ name: 'slideshow' }">Slideshow</RouterLink>
 		</nav>
 	
 	 <Routerview />
