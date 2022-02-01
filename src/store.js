b9e7b8af5a4c4c468232797e090c463b4f50218e
mimmimiro <amini.maryam@gmail.com>
2022-02-01 12:24:47 +0100

    basic store

--- src/store.js
+++ src/store.js
@@ -1,49 +1,7 @@
 export default {
 	state() {
 		return {
-			 quiz: [
-				{
-				  speed: 50,
-				  answer1:40 ,
-				  answer2: 20,
-				  answer3: 70,
-				  correct_answer: 20
-				},
-				{
-				  speed: 20,
-				  answer1: 10,
-				  answer2: 40,
-				  answer3: 30,
-				  correct_answer: 30
-				},
-				{
-				  speed: 80,
-				  answer1: 90,
-				  answer2: 30,
-				  answer3: 20,
-				  correct_answer: 30
-				},
-				{
-				  speed: 60,
-				  answer1: 40,
-				  answer2: 60,
-				  answer3: 80,
-				  correct_answer: 40
-				},
-				{
-				  speed: 10,
-				  answer1: 20,
-				  answer2: 100,
-				  answer3: 30,
-				  correct_answer: 100
-				},
-			 ]
-			 
-		}
-	},
-	 getters: {
-		 quiz(state) {
-			 return state.quiz;
-		 }
+			
 	 }
+	}
 };
